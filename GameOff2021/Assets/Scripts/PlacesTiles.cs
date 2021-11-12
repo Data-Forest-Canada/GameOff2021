8c89cf4673fb40316e2f53258aa9b48701c56314
Kevin Clarke <kevinclarke@kevinclarke.net>
2021-11-12 18:06:02 -0500

    Saving recipes(?)

--- GameOff2021/Assets/Scripts/PlacesTiles.cs
+++ GameOff2021/Assets/Scripts/PlacesTiles.cs
@@ -16,10 +16,11 @@ public class PlacesTiles : MonoBehaviour
     public void PlaceTileAtCell(Vector3Int cell)
     {
         //Debug.Log(tilemap.GetTile<Tile>(cell));
+        Debug.Log(toPlace);
         Tile atLocation = tilemap.GetTile<Tile>(cell);
         Tile newTile = atLocation?.CombineWith(toPlace) ?? atLocation;
         tilemap.SetTile(cell, newTile);
-        Debug.Log($"{toPlace} + {atLocation} = {newTile}");
+        //Debug.Log($"{toPlace} + {atLocation} = {newTile}");
     }
 
     bool isLeftClicking() => Input.GetMouseButtonDown(0);
@@ -31,6 +32,8 @@ public class PlacesTiles : MonoBehaviour
     {
         // Is this for a cooldown on placing tiles? If so, my Timer class would help make this easier B)
         // I'll put it into the project just in case - Brad
+
+        //Yes, this is a timer. Go ahead and change it if you have a better method. - Kevin
         if (!active)
         {
             delta = 1;
