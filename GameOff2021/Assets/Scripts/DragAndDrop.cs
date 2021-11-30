05ad9781ab1cd661c720f8bd4b8279b0f73f085a
Brad Oosterbroek <bradoosterbroek@gmail.com>
2021-11-30 17:32:54 -0500

    safety push

--- GameOff2021/Assets/Scripts/DragAndDrop.cs
+++ GameOff2021/Assets/Scripts/DragAndDrop.cs
@@ -144,7 +144,6 @@ public class DragAndDrop : MonoBehaviour
         // Previously this was checking to see if you used all your pieces. I don't think we want that to be a condition of winning so I removed it
         if (LevelComplete())
         {
-            Debug.Log("Invoking OnLevelComplete event.");
             GameManager.Instance.UnlockNextLevel();
             OnLevelComplete?.Invoke();
         }
